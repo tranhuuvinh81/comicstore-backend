8d9268941b261dfa5ee58d616c67e283caa79366
tranhuuvinh81 <vinht8802@gmail.com>
2025-04-30 17:42:59 +0700

    Update db config to connect to Railway

--- config/db.js
+++ config/db.js
@@ -1,23 +1,42 @@
+// // const mysql = require("mysql2/promise");
+
+// // const db = mysql.createPool({
+// //   host: "localhost",
+// //   user: "root", // Thay bằng username MySQL của bạn
+// //   password: "", // Thay bằng password MySQL của bạn
+// //   database: "tt",
+// // });
+
+// // module.exports = db;
+
 // const mysql = require("mysql2/promise");
 
 // const db = mysql.createPool({
-//   host: "localhost",
-//   user: "root", // Thay bằng username MySQL của bạn
-//   password: "", // Thay bằng password MySQL của bạn
-//   database: "tt",
+//   host: "hopper.proxy.rlwy.net", // host từ Railway
+//   user: "root",                  // user từ Railway
+//   password: "yuBOgMLnnNAIEfFZlCToTGnmjCg0tHlZ", // mật khẩu từ Railway
+//   database: "railway",           // tên DB từ Railway
+//   port: 28561                    // cổng từ Railway
 // });
 
 // module.exports = db;
 
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
-  host: "hopper.proxy.rlwy.net", // host từ Railway
-  user: "root",                  // user từ Railway
-  password: "yuBOgMLnnNAIEfFZlCToTGnmjCg0tHlZ", // mật khẩu từ Railway
-  database: "railway",           // tên DB từ Railway
-  port: 28561                    // cổng từ Railway
+const db = mysql.createConnection({
+  host: 'hopper.proxy.rlwy.net',
+  user: 'root',
+  password: 'yubOgMLnnNAIEfzZCIoTQGmnjCg0tHlZ',
+  database: 'railway',
+  port: 28561
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err.stack);
+    return;
+  }
+  console.log('Connected to the database.');
 });
 
 module.exports = db;
-
