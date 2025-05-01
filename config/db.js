f776ec2f25223656350b67e9f6a97f1a1082e96c
tranhuuvinh81 <vinht8802@gmail.com>
2025-05-01 20:37:05 +0700

    fix db

--- config/db.js
+++ config/db.js
@@ -9,16 +9,15 @@
 
 // module.exports = db;
 
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
-const db = mysql.createPool({
-  host: "hopper.proxy.rlwy.net", // host từ Railway
-  user: "root",                  // user từ Railway
-  password: "yubOgMLnnNAIEfzZCIoTQGmnjCgOtHlZ", // mật khẩu từ Railway
-  database: "railway",           // tên DB từ Railway
-  port: 3306                    // cổng từ Railway
-});
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+}).promise();
 
-module.exports = db;
 
 
