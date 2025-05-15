e42bb7ee81bcfbed31524050edea01c86c85fa00
tranhuuvinh81 <vinht8802@gmail.com>
2025-05-15 22:09:07 +0700

    fix

--- config/db.js
+++ config/db.js
@@ -1,26 +1,29 @@
-// const mysql = require("mysql2/promise");
-
-// const db = mysql.createPool({
-//   host: "localhost",
-//   user: "root", // Thay bằng username MySQL của bạn
-//   password: "", // Thay bằng password MySQL của bạn
-//   database: "tt",
-// });
-
-// module.exports = db;
-
 const mysql = require("mysql2/promise");
-require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: "localhost",
+  user: "root", // Thay bằng username MySQL của bạn
+  password: "", // Thay bằng password MySQL của bạn
+  database: "tt",
 });
 
 module.exports = db;
+
+// const mysql = require('mysql2/promise');
+
+// const connection = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected to MySQL');
+// });
+
+// module.exports = connection;
 
 
 
